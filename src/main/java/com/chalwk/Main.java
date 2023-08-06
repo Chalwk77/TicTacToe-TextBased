10a10880bcd1a2d1cceb4071199782ebebe57972
Chalwk77 <jericho.crosby227@gmail.com>
2023-08-06 22:57:10 +1200

    ...

--- src/main/java/com/chalwk/Main.java
+++ src/main/java/com/chalwk/Main.java
@@ -10,23 +10,87 @@
 
 package com.chalwk;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+    private final static String[] columns = {"A", "B", "C", "D", "E"};
+    private final static String[] rows = {"1", "2", "3", "4", "5"};
     public static String header = "";
+    static char player1 = 'X';
+    static char player2 = 'O';
+    static char empty = '-';
+    static char[][][] boards = {
+
+            // 3x3
+            {
+                    {empty, empty, empty},
+                    {empty, empty, empty},
+                    {empty, empty, empty}
+            },
+
+            // 4x4
+            {
+                    {empty, empty, empty, empty},
+                    {empty, empty, empty, empty},
+                    {empty, empty, empty, empty},
+                    {empty, empty, empty, empty}
+            },
+
+            // 5x5
+            {
+                    {empty, empty, empty, empty, empty},
+                    {empty, empty, empty, empty, empty},
+                    {empty, empty, empty, empty, empty},
+                    {empty, empty, empty, empty, empty},
+                    {empty, empty, empty, empty, empty}
+            }
+    };
+    static Map<Integer, String> map;
 
     public static void main(String[] args) {
 
+        map = new HashMap<>();
+
+        for (int i = 0; i < columns.length; i++) {
+            String column = columns[i];
+            map.put(i, column);
+        }
+        for (int i = 0; i < rows.length; i++) {
+            String row = rows[i];
+            map.put(i + columns.length, row);
+        }
+
+        String input = "C3";
+
+        for (int i = 0; i < map.size(); i++) {
+            String value = map.get(i);
+            System.out.println(i + " = " + value);
+        }
+
+
         Scanner scanner = new Scanner(System.in);
 
-        char[][] board = {
-                {'-', '-', '-'},
-                {'-', '-', '-'},
-                {'-', '-', '-'}};
+        char[][] board;
 
         showStatus(0);
+        print("Pick a board size: 3x3, 4x4, 5x5 (1, 2, 3 respectively)");
+        while (true) {
+
+            int userInput = scanner.nextInt();
+            if (userInput <= 0 || userInput > boards.length) {
+                print("Invalid input");
+                continue;
+            }
+            board = boards[userInput - 1];
+            break;
+        }
+
+        scanner = new Scanner(System.in);
+
         printBoard(board, false);
 
         while (true) {
@@ -67,17 +131,17 @@ public class Main {
 
     private static boolean isGameFinished(char[][] board) {
 
-        if (getWinner(board, 'X')) {
+        if (getWinner(board, player1)) {
             showStatus(1);
             return true;
-        } else if (getWinner(board, 'O')) {
+        } else if (getWinner(board, player2)) {
             showStatus(2);
             return true;
         }
 
         for (char[] chars : board) {
             for (char aChar : chars) {
-                if (aChar == '-') {
+                if (aChar == empty) {
                     return false;
                 }
             }
@@ -88,16 +152,108 @@ public class Main {
     }
 
     private static boolean getWinner(char[][] board, char symbol) {
-        return (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol) ||
-                (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol) ||
-                (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol) ||
 
-                (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol) ||
-                (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol) ||
-                (board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol) ||
+        switch (board.length) {
+            case 3 -> {
+                return (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol) ||
+                        (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol) ||
+                        (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol) ||
+                        (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol) ||
+                        (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol) ||
+                        (board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol) ||
+                        (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
+                        (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol);
+            }
+            case 4 -> {
+                return (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol && board[0][3] == symbol) ||
+                        (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol && board[1][3] == symbol) ||
+                        (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol && board[2][3] == symbol) ||
+                        (board[3][0] == symbol && board[3][1] == symbol && board[3][2] == symbol && board[3][3] == symbol) ||
+                        (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol && board[3][0] == symbol) ||
+                        (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol && board[3][1] == symbol);
+            }
+            case 5 -> {
+                return (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol && board[0][3] == symbol && board[0][4] == symbol) ||
+                        (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol && board[1][3] == symbol && board[1][4] == symbol) ||
+                        (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol && board[2][3] == symbol && board[2][4] == symbol) ||
+                        (board[3][0] == symbol && board[3][1] == symbol && board[3][2] == symbol && board[3][3] == symbol && board[3][4] == symbol) ||
+                        (board[4][0] == symbol && board[4][1] == symbol && board[4][2] == symbol && board[4][3] == symbol && board[4][4] == symbol) ||
+                        (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol && board[3][0] == symbol && board[4][0] == symbol) ||
+                        (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol && board[3][1] == symbol && board[4][1] == symbol) ||
+                        (board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol && board[3][2] == symbol && board[4][2] == symbol) ||
+                        (board[0][3] == symbol && board[1][3] == symbol && board[2][3] == symbol && board[3][3] == symbol && board[4][3] == symbol) ||
+                        (board[0][4] == symbol && board[1][4] == symbol && board[2][4] == symbol && board[3][4] == symbol && board[4][4] == symbol) ||
+                        (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol && board[3][3] == symbol && board[4][4] == symbol) ||
+                        (board[0][4] == symbol && board[1][3] == symbol && board[2][2] == symbol && board[3][1] == symbol && board[4][0] == symbol);
+            }
+        }
 
-                (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
-                (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol);
+        return false;
+    }
+
+    private static boolean isValidMove(char[][] board, String pos) {
+
+        switch (board.length) {
+            case 3 -> {
+
+                return (pos.equals("1") && board[0][0] == empty) ||
+                        (pos.equals("2") && board[0][1] == empty) ||
+                        (pos.equals("3") && board[0][2] == empty) ||
+                        (pos.equals("4") && board[1][0] == empty) ||
+                        (pos.equals("5") && board[1][1] == empty) ||
+                        (pos.equals("6") && board[1][2] == empty) ||
+                        (pos.equals("7") && board[2][0] == empty) ||
+                        (pos.equals("8") && board[2][1] == empty) ||
+                        (pos.equals("9") && board[2][2] == empty);
+            }
+            case 4 -> {
+                return (pos.equals("1") && board[0][0] == empty) ||
+                        (pos.equals("2") && board[0][1] == empty) ||
+                        (pos.equals("3") && board[0][2] == empty) ||
+                        (pos.equals("4") && board[0][3] == empty) ||
+                        (pos.equals("5") && board[1][0] == empty) ||
+                        (pos.equals("6") && board[1][1] == empty) ||
+                        (pos.equals("7") && board[1][2] == empty) ||
+                        (pos.equals("8") && board[1][3] == empty) ||
+                        (pos.equals("9") && board[2][0] == empty) ||
+                        (pos.equals("10") && board[2][1] == empty) ||
+                        (pos.equals("11") && board[2][2] == empty) ||
+                        (pos.equals("12") && board[2][3] == empty) ||
+                        (pos.equals("13") && board[3][0] == empty) ||
+                        (pos.equals("14") && board[3][1] == empty) ||
+                        (pos.equals("15") && board[3][2] == empty) ||
+                        (pos.equals("16") && board[3][3] == empty);
+            }
+            case 5 -> {
+                return (pos.equals("1") && board[0][0] == empty) ||
+                        (pos.equals("2") && board[0][1] == empty) ||
+                        (pos.equals("3") && board[0][2] == empty) ||
+                        (pos.equals("4") && board[0][3] == empty) ||
+                        (pos.equals("5") && board[0][4] == empty) ||
+                        (pos.equals("6") && board[1][0] == empty) ||
+                        (pos.equals("7") && board[1][1] == empty) ||
+                        (pos.equals("8") && board[1][2] == empty) ||
+                        (pos.equals("9") && board[1][3] == empty) ||
+                        (pos.equals("10") && board[1][4] == empty) ||
+                        (pos.equals("11") && board[2][0] == empty) ||
+                        (pos.equals("12") && board[2][1] == empty) ||
+                        (pos.equals("13") && board[2][2] == empty) ||
+                        (pos.equals("14") && board[2][3] == empty) ||
+                        (pos.equals("15") && board[2][4] == empty) ||
+                        (pos.equals("16") && board[3][0] == empty) ||
+                        (pos.equals("17") && board[3][1] == empty) ||
+                        (pos.equals("18") && board[3][2] == empty) ||
+                        (pos.equals("19") && board[3][3] == empty) ||
+                        (pos.equals("20") && board[3][4] == empty) ||
+                        (pos.equals("21") && board[4][0] == empty) ||
+                        (pos.equals("22") && board[4][1] == empty) ||
+                        (pos.equals("23") && board[4][2] == empty) ||
+                        (pos.equals("24") && board[4][3] == empty) ||
+                        (pos.equals("25") && board[4][4] == empty);
+            }
+        }
+
+        return false;
     }
 
     private static void computerTurn(char[][] board) {
@@ -107,22 +263,7 @@ public class Main {
             computerMove = rand.nextInt(9) + 1;
         } while (!isValidMove(board, Integer.toString(computerMove)));
         header = "Computer chose " + computerMove;
-        placeMove(board, Integer.toString(computerMove), 'O');
-    }
-
-    private static boolean isValidMove(char[][] board, String position) {
-        return switch (position) {
-            case "1" -> (board[0][0] == '-');
-            case "2" -> (board[0][1] == '-');
-            case "3" -> (board[0][2] == '-');
-            case "4" -> (board[1][0] == '-');
-            case "5" -> (board[1][1] == '-');
-            case "6" -> (board[1][2] == '-');
-            case "7" -> (board[2][0] == '-');
-            case "8" -> (board[2][1] == '-');
-            case "9" -> (board[2][2] == '-');
-            default -> false;
-        };
+        placeMove(board, Integer.toString(computerMove), player2);
     }
 
     private static void playerTurn(char[][] board, Scanner scanner) {
@@ -136,21 +277,77 @@ public class Main {
                 print(userInput + " is not a valid move.");
             }
         }
-        placeMove(board, userInput, 'X');
+        placeMove(board, userInput, player1);
     }
 
     private static void placeMove(char[][] board, String position, char symbol) {
-        switch (position) {
-            case "1" -> board[0][0] = symbol;
-            case "2" -> board[0][1] = symbol;
-            case "3" -> board[0][2] = symbol;
-            case "4" -> board[1][0] = symbol;
-            case "5" -> board[1][1] = symbol;
-            case "6" -> board[1][2] = symbol;
-            case "7" -> board[2][0] = symbol;
-            case "8" -> board[2][1] = symbol;
-            case "9" -> board[2][2] = symbol;
-            default -> print("Invalid input");
+
+        switch (board.length) {
+            case 3 -> {
+                switch (position) {
+                    case "1" -> board[0][0] = symbol;
+                    case "2" -> board[0][1] = symbol;
+                    case "3" -> board[0][2] = symbol;
+                    case "4" -> board[1][0] = symbol;
+                    case "5" -> board[1][1] = symbol;
+                    case "6" -> board[1][2] = symbol;
+                    case "7" -> board[2][0] = symbol;
+                    case "8" -> board[2][1] = symbol;
+                    case "9" -> board[2][2] = symbol;
+                    default -> print("Invalid input");
+                }
+            }
+            case 4 -> {
+                switch (position) {
+                    case "1" -> board[0][0] = symbol;
+                    case "2" -> board[0][1] = symbol;
+                    case "3" -> board[0][2] = symbol;
+                    case "4" -> board[0][3] = symbol;
+                    case "5" -> board[1][0] = symbol;
+                    case "6" -> board[1][1] = symbol;
+                    case "7" -> board[1][2] = symbol;
+                    case "8" -> board[1][3] = symbol;
+                    case "9" -> board[2][0] = symbol;
+                    case "10" -> board[2][1] = symbol;
+                    case "11" -> board[2][2] = symbol;
+                    case "12" -> board[2][3] = symbol;
+                    case "13" -> board[3][0] = symbol;
+                    case "14" -> board[3][1] = symbol;
+                    case "15" -> board[3][2] = symbol;
+                    case "16" -> board[3][3] = symbol;
+                    default -> print("Invalid input");
+                }
+            }
+            case 5 -> {
+                switch (position) {
+                    case "1" -> board[0][0] = symbol;
+                    case "2" -> board[0][1] = symbol;
+                    case "3" -> board[0][2] = symbol;
+                    case "4" -> board[0][3] = symbol;
+                    case "5" -> board[0][4] = symbol;
+                    case "6" -> board[1][0] = symbol;
+                    case "7" -> board[1][1] = symbol;
+                    case "8" -> board[1][2] = symbol;
+                    case "9" -> board[1][3] = symbol;
+                    case "10" -> board[1][4] = symbol;
+                    case "11" -> board[2][0] = symbol;
+                    case "12" -> board[2][1] = symbol;
+                    case "13" -> board[2][2] = symbol;
+                    case "14" -> board[2][3] = symbol;
+                    case "15" -> board[2][4] = symbol;
+                    case "16" -> board[3][0] = symbol;
+                    case "17" -> board[3][1] = symbol;
+                    case "18" -> board[3][2] = symbol;
+                    case "19" -> board[3][3] = symbol;
+                    case "20" -> board[3][4] = symbol;
+                    case "21" -> board[4][0] = symbol;
+                    case "22" -> board[4][1] = symbol;
+                    case "23" -> board[4][2] = symbol;
+                    case "24" -> board[4][3] = symbol;
+                    case "25" -> board[4][4] = symbol;
+                    default -> print("Invalid input");
+                }
+            }
         }
     }
 
@@ -160,17 +357,48 @@ public class Main {
             print("\n".repeat(50));
         }
 
-        print(board[0][0] + "  | " + board[0][1] + " | " + board[0][2]);
-        print("---+---+---");
-        print(board[1][0] + "  | " + board[1][1] + " | " + board[1][2]);
-        print("---+---+---");
-        print(board[2][0] + "  | " + board[2][1] + " | " + board[2][2]);
+        switch (board.length) {
+            case 3 -> {
+                print("  --A---B---C--");
+                print("1 | " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |");
+                print("  |---+---+---|");
+                print("2 | " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |");
+                print("  |---+---+---|");
+                print("3 | " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |");
+                print("  |-----------|");
+            }
+            case 4 -> {
+                // print with ABC 1234
+                print("  --A---B---C---D--");
+                print("1 | " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " | " + board[0][3] + " |");
+                print("  |---+---+---+---|");
+                print("2 | " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " |");
+                print("  |---+---+---+---|");
+                print("3 | " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " |");
+                print("  |---+---+---+---|");
+                print("4 | " + board[3][0] + " | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " |");
+                print("  |---------------|");
+            }
+            case 5 -> {
+                print("  --A---B---C---D---E--");
+                print("1 | " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " | " + board[0][3] + " | " + board[0][4] + " |");
+                print("  |---+---+---+---+---|");
+                print("2 | " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " | " + board[1][4] + " |");
+                print("  |---+---+---+---+---|");
+                print("3 | " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " | " + board[2][4] + " |");
+                print("  |---+---+---+---+---|");
+                print("4 | " + board[3][0] + " | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " | " + board[3][4] + " |");
+                print("  |---+---+---+---+---|");
+                print("5 | " + board[4][0] + " | " + board[4][1] + " | " + board[4][2] + " | " + board[4][3] + " | " + board[4][4] + " |");
+                print("  |-------------------|");
+            }
+        }
 
         StringBuilder available = new StringBuilder("Make your move: ");
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == '-') {
+                if (board[i][j] == empty) {
                     if (i == board.length - 1 && j == board[i].length - 1) {
                         available.append((i * board.length + j + 1));
                         break;
