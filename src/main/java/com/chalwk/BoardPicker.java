9adb3762727d6867aef334b4c2215642930687e5
Chalwk77 <jericho.crosby227@gmail.com>
2023-08-08 09:44:40 +1200

    ..

--- src/main/java/com/chalwk/BoardPicker.java
+++ src/main/java/com/chalwk/BoardPicker.java
@@ -55,13 +55,11 @@ public class BoardPicker {
                     "Y", "Z"
             }, 0, board.length);
 
-            for (int row = 0; row < board.length; row++) {
-                String letter = letters[row];
-                for (int col = 0; col < board.length; col++) {
-                    System.out.println(letter + (col + 1) + " [" + row + "-" + col + "]");
-                    cell_indicators.put(letter + (col + 1), new int[]{row, col});
-                }
-            }
+             for (int row = 0; row < board.length; row++) {
+                 for (int col = 0; col < board.length; col++) {
+                     cell_indicators.put(letters[row] + (col + 1), new int[]{col, row});
+                 }
+             }
 
             break;
         }
