23cd6d3be582740a3d4bc2cedf711e59bb104eaa
Chalwk77 <jericho.crosby227@gmail.com>
2023-08-08 16:06:25 +1200

    ...

--- src/main/java/com/chalwk/PrintBoard.java
+++ src/main/java/com/chalwk/PrintBoard.java
@@ -31,85 +31,96 @@ public class PrintBoard {
             case 9 -> board8(board);
             default -> throw new IllegalStateException("Board size not supported: (" + len + "x" + len + ")");
         }
-        printDivider(board);
         printCellIndicators(board);
     }
 
     private static void board1(char[][] b) {
         for (int i = 0; i < 2; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " |");
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " |");
+            if (i != 1) {
+                print("  |---+---|");
+            }
         }
     }
 
     private static void board2(char[][] b) {
         for (int i = 0; i < 3; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]);
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2] + " |");
+            if (i != 2) {
+                print("  |---+---+---|");
+            }
         }
     }
 
     private static void board3(char[][] b) {
         for (int i = 0; i < 4; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
-                    + " | " + b[i][3]);
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
+                    + " | " + b[i][3] + " |");
+            if (i != 3) {
+                print("  |---+---+---+---|");
+            }
         }
     }
 
     private static void board4(char[][] b) {
         for (int i = 0; i < 5; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
-                    + " | " + b[i][3] + " | " + b[i][4]);
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
+                    + " | " + b[i][3] + " | " + b[i][4] + " |");
+            if (i != 4) {
+                print("  |---+---+---+---+---|");
+            }
         }
     }
 
     private static void board5(char[][] b) {
         for (int i = 0; i < 6; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
-                    + " | " + b[i][3] + " | " + b[i][4] + " | " + b[i][5]);
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
+                    + " | " + b[i][3] + " | " + b[i][4] + " | " + b[i][5] + " |");
+            if (i != 5) {
+                print("  |---+---+---+---+---+---|");
+            }
         }
     }
 
     private static void board6(char[][] b) {
         for (int i = 0; i < 7; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
                     + " | " + b[i][3] + " | " + b[i][4] + " | " + b[i][5]
-                    + " | " + b[i][6]);
+                    + " | " + b[i][6] + " |");
+            if (i != 6) {
+                print("  |---+---+---+---+---+---+---|");
+            }
         }
     }
 
     private static void board7(char[][] b) {
         for (int i = 0; i < 8; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
                     + " | " + b[i][3] + " | " + b[i][4] + " | " + b[i][5]
-                    + " | " + b[i][6] + " | " + b[i][7]);
+                    + " | " + b[i][6] + " | " + b[i][7] + " |");
+            if (i != 7) {
+                print("  |---+---+---+---+---+---+---+---|");
+            }
         }
     }
 
     private static void board8(char[][] b) {
         for (int i = 0; i < 9; i++) {
-            print((i + 1) + " " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
+            print((i + 1) + " | " + b[i][0] + " | " + b[i][1] + " | " + b[i][2]
                     + " | " + b[i][3] + " | " + b[i][4] + " | " + b[i][5]
-                    + " | " + b[i][6] + " | " + b[i][7] + " | " + b[i][8]);
+                    + " | " + b[i][6] + " | " + b[i][7] + " | " + b[i][8] + " |");
+            if (i != 8) {
+                print("  |---+---+---+---+---+---+---+---+---|");
+            }
         }
     }
 
     public static void printLetters(char[][] board) {
-        StringBuilder chars = new StringBuilder("  ");
+        StringBuilder chars = new StringBuilder("    ");
         for (int i = 0; i < board.length; i++) {
             chars.append(letters[i]).append("   ");
         }
         print(chars.toString());
-    }
-
-    public static void printDivider(char[][] board) {
-
-    /*
-          A   B   C
-        1 - | - | -
-          --+---+--
-        2 - | X | -
-          --+---+--
-        3 - | - | -
-    */
     }
 
     /***
